a570eff048f41d5ee25c0f93726ff35c5e7416a8
Puzanovim <puzanovim@yandex.ru>
2020-02-10 18:47:07 +0500

    add admin

--- bboard/views.py
+++ bboard/views.py
@@ -7,7 +7,7 @@ from .models import Bb
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
 
     # template = loader.get_template('bboard/index.html')
