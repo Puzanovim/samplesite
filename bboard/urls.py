dba4a0d35f8a143ecc3d59320e5fad2450525ef5
Puzanovim <puzanovim@yandex.ru>
2020-02-10 20:30:40 +0500

    add adding form

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import index, hello
+from .views import index, hello, by_rubric
+from .views import BbCreateView
 
 urlpatterns = [
-    path('', index),
-    path('hello/', hello),
+    path('', index, name='index'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubrics'),
+    path('add/', BbCreateView.as_view(), name='add'),
+    path('hello/', hello, name='hello'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .forms import BbForm
 
 from .models import Bb
+from .models import Rubric
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
 
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
 
     # template = loader.get_template('bboard/index.html')
     # bbs = Bb.objects.order_by('-published')
@@ -23,5 +48,3 @@ def index(request):
 
 def hello(request):
     return HttpResponse("Hello!")
-
-# Create your views here.
