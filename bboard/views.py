48e3deb487ce683b964dbd29ac35f05cacebafb1
Puzanovim <puzanovim@yandex.ru>
2020-02-10 14:45:04 +0500

    add templates

--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Bb
 
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context, request))
+
+    # s = 'Список объявлений\r\n\r\n\r\n'
+    # for bb in Bb.objects.order_by('-published'):
+    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    # return HttpResponse(s, content_type='text/plain; charset=utf-8')
 
 
 def hello(request):
